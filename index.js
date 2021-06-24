5766da76e197f127f8d89870b759daa423dc2bdd
Ihor Solomaka <igor.solomakha@gmail.com>
2021-06-24 16:12:12 +0300

    feat: add plugins input

--- index.js
+++ index.js
@@ -7,19 +7,16 @@ const rng = require('@semantic-release/release-notes-generator')
 ;(async function() {
   const releaseBranches = core.getInput('release-branches').split('\n');
   const prereleaseBranches = core.getInput('prerelease-branches').split('\n').map(name => ({ name, prerelease: name.split(':')[1] || true }));
+  const plugins = core.getInput('plugins').split('\n');
 
-  console.log({
-    releaseBranches,
-    prereleaseBranches
-  })
-
-  const result = await sr({
+  const config = {
     branches: [...releaseBranches, ...prereleaseBranches],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator'
-    ]
-  })
+    plugins
+  }
+
+  console.log('Configuration', config)
+
+  const result = await sr(config)
   
   if (!result) throw new Error('No release happened')
   core.setOutput('version', result.nextRelease.version)
